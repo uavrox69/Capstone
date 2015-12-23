b1e109a308c142c185999428f2c97c9686ab874f
joseph yslas <jyslas@hotmail.com>
2015-12-22 16:29:07 -0800

    added create account xml, some code for main activity

---
+++ app/src/main/java/yslas/joseph/memoryjournal/CreateAccount.java
@@ -0,0 +1,19 @@
+package yslas.joseph.memoryjournal;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by JYsla_000 on 12/22/2015.
+ */
+public class CreateAccount extends  MainActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+
+    }
+}
--- app/src/main/java/yslas/joseph/memoryjournal/MainActivity.java
+++ app/src/main/java/yslas/joseph/memoryjournal/MainActivity.java
@@ -1,9 +1,12 @@
 package yslas.joseph.memoryjournal;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +14,15 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_screen);
+        Button create = (Button)(findViewById(R.id.create_account));
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CreateAccount.class));
+            }
+        });
     }
 
 
